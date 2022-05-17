69df06d74d2e5ba544ca7181ef2c88c6e74b2754
Dzmitry Kishylau <dkishylau@gmail.com>
2022-05-16 22:09:35 -0700

    Add a shortcut to display current device and fix the problem with default effect device

--- init.lua
+++ init.lua
@@ -5,7 +5,10 @@ function setMacAsDefaultAudioDevice()
    mic = hs.audiodevice.findDeviceByUID("BuiltInMicrophoneDevice") -- use UID instead of name since it's more stable
    if mic then mic:setDefaultInputDevice() end
    speakers = hs.audiodevice.findDeviceByUID("BuiltInSpeakerDevice")
-   if speakers then speakers:setDefaultOutputDevice() end
+   if speakers then
+      speakers:setDefaultOutputDevice()
+      speakers:setDefaultEffectDevice()
+   end
    if speakers and mic then
       hs.alert.show("Changed input and output to Mac")
    else
@@ -17,7 +20,10 @@ function setHeadphonesAsDefaultAudioDevice()
    headphonesIn = hs.audiodevice.findInputByName("WH-H900N (h.ear)")
    headphonesOut = hs.audiodevice.findOutputByName("WH-H900N (h.ear)")
    if headphonesIn then headphonesIn:setDefaultInputDevice() end
-   if headphonesOut then headphonesOut:setDefaultOutputDevice() end
+   if headphonesOut then
+      headphonesOut:setDefaultOutputDevice()
+      headphonesOut:setDefaultEffectDevice()
+   end
    if headphonesIn and headphonesOut then
       hs.alert.show("Changed input and output to Sony headphones")
    else
@@ -30,12 +36,22 @@ function setScarlettAsDefaultAudioDevice()
    if scarlett then
       scarlett:setDefaultInputDevice()
       scarlett:setDefaultOutputDevice()
+      scarlett:setDefaultEffectDevice()
       hs.alert.show("Changed input and output to Scarlett")
    else
       hs.alert.show("Couldn't change input and output to Scarlett")
    end
 end
 
+function displayCurrentAudioDevices()
+   input = hs.audiodevice.defaultInputDevice()
+   if input then inputName = input:name() else inputName = "None" end
+   output = hs.audiodevice.defaultOutputDevice()
+   if output then outputName = output:name() else outputName = "None" end
+   hs.alert.show(string.format("Input: %s | Output: %s", inputName, outputName))
+end
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", setMacAsDefaultAudioDevice)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", setHeadphonesAsDefaultAudioDevice)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", setScarlettAsDefaultAudioDevice)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "A", displayCurrentAudioDevices)
